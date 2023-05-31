e2e300c4446049d17531f57940b1e78cbf94b673
ElumionJ <vladyslav.d@mgroupweb.com>
2023-05-31 09:54:14 +0300

    add styles for heading

--- app/routes/($locale).products.$productHandle.tsx
+++ app/routes/($locale).products.$productHandle.tsx
@@ -125,47 +125,51 @@ export default function Product() {
 
   return (
     <>
-      <Section className="px-0 md:px-8 lg:px-12">
-        <div className="grid items-start md:gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
+      <Section className=" justify-center !gap-[63px] !flex px-0 md:px-8 lg:px-12 ">
+        <div className=" px-[4px] max-w-[325px]">
+          <Heading as="h1" className="text-5xl  mb-8">
+            {title}
+          </Heading>
+          {descriptionHtml && (
+            <p
+              className="text-[16px] text-[#333333]"
+              dangerouslySetInnerHTML={{__html: descriptionHtml}}
+            ></p>
+          )}
+        </div>
+        <div className="w-[350px]">
           <ProductGallery
             media={media.nodes}
             className="w-full lg:col-span-2"
           />
-          <div className="sticky md:-mb-nav md:top-nav md:-translate-y-nav md:h-screen md:pt-nav hiddenScroll md:overflow-y-scroll">
-            <section className="flex flex-col w-full max-w-xl gap-8 p-6 md:mx-auto md:max-w-sm md:px-0">
-              <div className="grid gap-2">
-                <Heading as="h1" className="whitespace-normal">
-                  {title}
-                </Heading>
-                {vendor && (
-                  <Text className={'opacity-50 font-medium'}>{vendor}</Text>
-                )}
+        </div>
+
+        <div className="sticky md:-mb-nav md:top-nav md:-translate-y-nav md:pt-nav hiddenScroll md:overflow-y-scroll">
+          <section className="flex flex-col w-full max-w-xl gap-8 p-6 md:mx-auto md:max-w-sm md:px-0">
+            {vendor && (
+              <div className="flex gap-[8px] items-end">
+                <span className="text-xl text-[#000] uppercase">Brand:</span>
+                <span className="text-[#333]">{vendor}</span>
               </div>
-              <ProductForm />
-              <div className="grid gap-4 py-4">
-                {descriptionHtml && (
-                  <ProductDetail
-                    title="Product Details"
-                    content={descriptionHtml}
-                  />
-                )}
-                {shippingPolicy?.body && (
-                  <ProductDetail
-                    title="Shipping"
-                    content={getExcerpt(shippingPolicy.body)}
-                    learnMore={`/policies/${shippingPolicy.handle}`}
-                  />
-                )}
-                {refundPolicy?.body && (
-                  <ProductDetail
-                    title="Returns"
-                    content={getExcerpt(refundPolicy.body)}
-                    learnMore={`/policies/${refundPolicy.handle}`}
-                  />
-                )}
-              </div>
-            </section>
-          </div>
+            )}
+            <ProductForm />
+            <div className="grid gap-4 py-4">
+              {shippingPolicy?.body && (
+                <ProductDetail
+                  title="Shipping"
+                  content={getExcerpt(shippingPolicy.body)}
+                  learnMore={`/policies/${shippingPolicy.handle}`}
+                />
+              )}
+              {refundPolicy?.body && (
+                <ProductDetail
+                  title="Returns"
+                  content={getExcerpt(refundPolicy.body)}
+                  learnMore={`/policies/${refundPolicy.handle}`}
+                />
+              )}
+            </div>
+          </section>
         </div>
       </Section>
       <Suspense fallback={<Skeleton className="h-32" />}>
