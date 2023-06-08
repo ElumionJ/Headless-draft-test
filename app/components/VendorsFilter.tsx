d7602a2c1ce497f68d2a3b1a72632494076c8fbc
Vlad Dovhaliuk <vladyslav.d@mgroupweb.com>
2023-06-08 18:03:08 +0300

    worked on logic of component

--- app/components/VendorsFilter.tsx
+++ app/components/VendorsFilter.tsx
@@ -1,4 +1,6 @@
-import React, {useRef, useState} from 'react';
+import {useNavigate, useNavigation} from '@remix-run/react';
+import React, {useEffect, useRef, useState} from 'react';
+import {HiArrowSmDown} from 'react-icons/hi';
 
 import useOutsideClick from '~/hooks/useOutsideClick';
 
@@ -9,28 +11,63 @@ interface Props {
       quantity: number;
     };
   };
+  url: string;
+  click: (value: string, action: string) => void;
 }
 
-export const VendorsFilter = ({vendors}: Props) => {
+export const VendorsFilter = ({vendors, click, url}: Props) => {
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const brandsFilterRef = useRef(null);
   useOutsideClick(brandsFilterRef, setOpen);
+
   return (
-    <div ref={brandsFilterRef}>
+    <div ref={brandsFilterRef} className="relative z-20">
       <button
-        className="uppercase"
+        className={`${
+          open ? 'bg-c-red text-[#fff]' : 'bg-c-gray text-[#000]'
+        } uppercase px-[24px] py-[13px] rounded-[100px] flex justify-between font-bebas text-[20px] leading-[110%] tracking-wider gap-1 w-[180px]`}
         onClick={() => {
           setOpen((prevState) => !prevState);
         }}
       >
         Brands
+        <HiArrowSmDown className={`${open && 'rotate-180'} `} />
       </button>
-      <ul  className={`${open ? 'block' : 'hidden'}`}>
-        <li>AAA</li>
-        <li>AAA</li>
-        <li>AAA</li>
-        <li>AAA</li>
+      <ul
+        className={`${
+          open ? 'block' : 'hidden'
+        } absolute top-full left-0 bg-[#fff] w-fit p-[24px] font-noto shadow-darkPopUp mt-[13px]`}
+      >
+        {vendors &&
+          Object.entries(vendors).map((el) => (
+            <li key={el[0]} className="">
+              <label
+                htmlFor={el[1].title}
+                className=" flex items-center justify-between gap-[60px] py-[8px]"
+              >
+                <span className="min-w-[180px] font-noto leading-[150%]">
+                  {el[1].title} ({el[1].quantity})
+                </span>
+                <input
+                  onChange={(e) => {
+                    click(e.target.value, e.target.checked);
+                  }}
+                  defaultChecked={url.includes(el[0])}
+                  className="text-[#D80F16]"
+                  type="checkbox"
+                  value={el[0]}
+                  id={el[1].title}
+                />
+              </label>
+            </li>
+          ))}
       </ul>
     </div>
   );
 };
+
+// const handleCheckboxClick = () => {
+//   // Clear the existing timer if there is one
+
+// };
