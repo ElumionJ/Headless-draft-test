199d202de4be9918efd3b294db215591c7d7ebbb
Vitalii Skomskiy <skomskiy@ukr.net>
2023-06-22 17:36:28 +0300

    [fix](script):added image zoom on pdp

--- app/components/SwiperImages.tsx
+++ app/components/SwiperImages.tsx
@@ -1,6 +1,8 @@
 import {useEffect} from 'react';
 import Swiper from 'swiper';
 import {Image} from '@shopify/hydrogen';
+import React from 'react';
+import ImageZoom from 'react-image-zooom';
 
 interface Props {
   media: any[];
@@ -31,13 +33,25 @@ export const SwiperImages = ({media}: Props) => {
         <div className="swiper w-full">
           <div className="swiper-wrapper">
             {media.map((el) => (
-              <div key={el.id} className="swiper-slide">
-                <Image
+              <div key={el.id} className="swiper-slide aspect-[3/4]">
+                <div
+                  className="object-cover h-full"
+                  style={{backgroundImage: `url(${el?.image?.url})`}}
+                >
+                  <ImageZoom
+                    src={el?.image?.url || ''}
+                    alt=""
+                    zoom="250"
+                    className={`object-cover image-zoom fadeIn !w-full bg-inherit`}
+                  />
+                </div>
+
+                {/* <Image
                   aspectRatio="3/4"
                   loading={'eager'}
                   data={el.image!}
                   className="object-cover h-full fadeIn"
-                />
+                /> */}
               </div>
             ))}
           </div>
